8dfb2ee1feeb0295f19532f2737e28efecc047ce
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-04-28 22:15:58 +0100

    first functionality pass

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const myLibrary = [];
+const bookTitle = document.querySelector('#bookName').value;
+const bookAuthor = document.querySelector('#bookAuthor').value;
+const bookPages = document.querySelector('#bookPages').value;
+const bookRead = document.querySelector('#bookRead').value;
+const btnAddBook = document.querySelector('#btnAddBook');
+
+btnAddBook.addEventListener('click', pushBook);
 
 function Book(title, author, numPages, read) {
     this.title = title;
@@ -14,6 +21,15 @@ function Book(title, author, numPages, read) {
     }
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, numPages, read) {
+    let newBook = new Book(title, author, numPages, read);
+    myLibrary.push(newBook);
+}
+
+function pushBook() {
+    if (bookRead === true) {
+        addBookToLibrary(bookTitle, bookAuthor, bookPages, "Yes");
+    } else {
+        addBookToLibrary(bookTitle, bookAuthor, bookPages, "No");
+    }
 }
